fba54daa3be95da5bf920ef58ef9f59d1464ad41
Max <mlapsparetime@gmail.com>
2021-02-22 20:28:23 -0300

    Ej.D Completo y agrego Ej.E - problemas FULL JOIN

--- finalPrincipeMaximilianoEjericioD.sql
+++ finalPrincipeMaximilianoEjericioD.sql
@@ -2,3 +2,10 @@
 -- comercial y teléfono del consultorio y la cantidad de médicos que han alquilado ese
 -- consultorio. Si un consultorio no tiene alquileres debe aparecer en cero la cantidad.
 
+SELECT c.nombre_comercial, c.telefono, COUNT(idMedico) AS 'Cantidad Medicos'
+FROM medicos m 
+RIGHT JOIN alquiler_consultorios ac
+ON m.id_medico = ac.idMedico
+RIGHT JOIN consultorios c
+ON c.id_consultorio = ac.idConsultorio
+GROUP BY id_consultorio;
---
+++ finalPrincipeMaximilianoEjericioE.sql
@@ -0,0 +1,8 @@
+-- e. Todos los datos de los consultorios, los alquileres y los médicos. Si hay
+-- consultorios sin alquileres deben aparecer los datos del consultorio, dejando en
+-- blanco los datos de las otras tablas. El listado debe ordenarse por id del consultorio.
+-- Debe mostrar todos los datos como nombre comercial del consultorio, apellido y
+-- nombre del médico y la fecha_desde del alquiler del consultorio. Se espera que en
+-- esta consulta se muestren todos los registros insertados. Verificar que eso suceda.
+
+
